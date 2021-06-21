781830e3c5e44a46a653ee77bed2e4b4a943ef26
daniel <danielricardopinto@gmail.com>
2021-06-21 16:42:16 -0300

    Proyect PIL Login && Register

--- Pil-Pr/src/app/app-routing.module.ts
+++ Pil-Pr/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './administracion/login/login.component';
+import { RegistroComponent } from './administracion/registro/registro.component';
 
 const routes: Routes = [
-  {path:"",component:LoginComponent, pathMatch:"full"}
+  {path:"",component:LoginComponent, pathMatch:"full"},
+  {path:"registro",component:RegistroComponent, pathMatch:"full"}
 ];
 
 @NgModule({
