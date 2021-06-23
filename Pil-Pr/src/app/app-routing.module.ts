48e4b5d0339664113ad045a796fcca12e87cd5c1
Hakkunito <84341878+Hakkunito@users.noreply.github.com>
2021-06-23 12:55:06 -0300

    DashBoard

--- Pil-Pr/src/app/app-routing.module.ts
+++ Pil-Pr/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './administracion/dashboard/dashboard.component';
 import { LoginComponent } from './administracion/login/login.component';
 import { RegistroComponent } from './administracion/registro/registro.component';
 
 const routes: Routes = [
   {path:"",component:LoginComponent, pathMatch:"full"},
-  {path:"registro",component:RegistroComponent, pathMatch:"full"}
+  {path:"registro",component:RegistroComponent, pathMatch:"full"},
+  {path:"dashboard",component:DashboardComponent, pathMatch:"full"}
 ];
 
 @NgModule({
