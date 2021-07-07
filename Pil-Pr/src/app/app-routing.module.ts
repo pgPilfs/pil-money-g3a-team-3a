d8df3e308563cc48d3184042af58914f23e8621c
JereA00 <jeremias_davidenco@hotmail.com>
2021-07-07 12:52:32 -0300

    Estructura del proyecto para aplicar template

--- Pil-Pr/src/app/app-routing.module.ts
+++ Pil-Pr/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './administracion/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 
 const routes: Routes = [
   {path:"login",component: LoginComponent, pathMatch:"full"},
-  {path:"registro",component:RegistroComponent, pathMatch:"full"},
-  {path:"home",component: DashboardComponent, pathMatch:"full"},
+  {path:"registro",component: RegistroComponent, pathMatch:"full"},
   {path: "**", redirectTo: "login", pathMatch: "full"}
 ];
 
--- Pil-Pr/src/app/components/registro/registro.component.ts
+++ Pil-Pr/src/app/components/registro/registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Archivo } from 'src/app/models/Archivo';
 
 @Component({
   selector: 'app-registro',
@@ -12,10 +13,13 @@ export class RegistroComponent implements OnInit {
   formulario : FormGroup;
   loading = false;
   submitted = false;
+  img_doc : any;
+  img_doc_2 : any;
+  aceptarTerminos: boolean = false;
 
   constructor(
     private fb: FormBuilder,
-    private router: Router
+    private router: Router    
     ) { 
     this.formulario = this.fb.group({
       firstName: ['', Validators.required],
@@ -23,9 +27,8 @@ export class RegistroComponent implements OnInit {
       dni: ['', [Validators.required, Validators.pattern('[0-9]{8}')]],
       mail: ['', [Validators.required, Validators.email]],
       username: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(8)]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
     });
-
   }
 
   ngOnInit(): void {
@@ -49,6 +52,41 @@ export class RegistroComponent implements OnInit {
     //Envio de formulario al backend 
 
     //Redigir a la pagina de login
+    //this.router.navigate(['/login']);
+  }
+  
+  fileEvent(imgInput: Event){
+    let file = (<HTMLInputElement>imgInput.target).files![0];
+    this.img_doc = new Archivo(1,file.name,file.type);
+  }
+
+  fileEvent2(imgInput: Event){
+    let file = (<HTMLInputElement>imgInput.target).files![0];
+    this.img_doc_2 = new Archivo(2,file.name,file.type);
+  }
+
+  subirArchivos(){
+    //Subir imagen 1
+    /*this.img.uploadFiles(this.img_doc).subscribe(data => {
+      console.log("se subio excelente la imagen 1")
+    })*/
+
+    //subir imagen 2
+    /*
+    this.img.uploadFiles(this.img_doc_2).subscribe(data => {
+      console.log("se subio excelente la imagen 2")
+    })
+    */
+    
+    console.log("Se subieron las imagenes")
+    console.log(this.img_doc);
+    console.log(this.img_doc_2)
+
+    //Redigir a la pagina de login
     this.router.navigate(['/login']);
+  }
+
+  aceparTerminosYCondiciones(){
+    this.aceptarTerminos = true;
   }
 }
