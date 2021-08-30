536627d1e2c966f80c519e41b0cf33f1801ff346
Lucía <luciagrosso.edu@gmail.com>
2021-08-29 22:45:15 -0300

    Crear carpeta Manual

--- BEPilMoney/BEPilMoney/Controllers/UsuarioController.cs
+++ BEPilMoney/BEPilMoney/Controllers/UsuarioController.cs
@@ -15,20 +15,20 @@ namespace BEPilMoney.Controllers
     {
         private UsuarioRepositorio _usuario = new UsuarioRepositorio();
         
-        public IHttpActionResult Get()
-        {
-            var listadoUsuario = this._usuario.Listado();
-            if (listadoUsuario == null || listadoUsuario.Rows.Count == 0) return NotFound();
-            return Ok(listadoUsuario);
-        }
+        //public IHttpActionResult Get()
+        //{
+        //    var listadoUsuario = this._usuario.Listado();
+        //    if (listadoUsuario == null || listadoUsuario.Rows.Count == 0) return NotFound();
+        //    return Ok(listadoUsuario);
+        //}
 
-        public IHttpActionResult Get(int id)
-        {
-            id = (id == 0) ? 0 : id;
-            var DetalleDelUsuario = this._usuario.Detalle(id);
-            if (DetalleDelUsuario == null || DetalleDelUsuario.Rows.Count == 0) return NotFound();
-            return Ok(DetalleDelUsuario);
-        }
+        //public IHttpActionResult Get(int id)
+        //{
+        //    id = (id == 0) ? 0 : id;
+        //    var DetalleDelUsuario = this._usuario.Detalle(id);
+        //    if (DetalleDelUsuario == null || DetalleDelUsuario.Rows.Count == 0) return NotFound();
+        //    return Ok(DetalleDelUsuario);
+        //}
 
         //public IHttpActionResult Post([FromBody] Usuario usuario)
         //{
@@ -46,12 +46,12 @@ namespace BEPilMoney.Controllers
         //    return Ok(resp);
         //}
 
-        public IHttpActionResult Delete(int id)
-        {
-            id = (id == 0) ? 0 : id;
-            var resp = this._usuario.Eliminar(id);
-            if (resp == 0) return BadRequest();
-            return Ok(resp);
-        }
+        //public IHttpActionResult Delete(int id)
+        //{
+        //    id = (id == 0) ? 0 : id;
+        //    var resp = this._usuario.Eliminar(id);
+        //    if (resp == 0) return BadRequest();
+        //    return Ok(resp);
+        //}
     }
 }
